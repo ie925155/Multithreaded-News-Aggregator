cfb2fe1dc2d866ccbb49b71cb10cb0b76daca442
Sheldon Lai <ie925155@gmail.com>
2016-10-25 16:20:43 +0000

    Init Commit

---
+++ Makefile
@@ -0,0 +1,79 @@
+#
+# Based on Makefiles previously written by Julie Zelenski.
+#
+
+CXX = g++
+
+# The CXXFLAGS variable sets compile flags for gcc:
+#  -g                         compile with debug information
+#  -Wall                      give all diagnostic warnings
+#  -pedantic                  require compliance with ANSI standard
+#  -O0                        do not optimize generated code
+#  -std=c++0x                 go with the c++0x extensions for thread support, unordered maps, etc
+#  -D_GLIBCXX_USE_NANOSLEEP   included for this_thread::sleep_for and this_thread::sleep_until support
+#  -D_GLIBCXX_USE_SCHED_YIELD included for this_thread::yield support
+#CXXFLAGS = -g -Wall -pedantic -O0 -std=c++0x -D_GLIBCXX_USE_NANOSLEEP -D_GLIBCXX_USE_SCHED_YIELD -I/usr/class/cs110/include/libxml2 -I/usr/class/cs110/local/include
+CXXFLAGS = -g -Wall -pedantic -O0 -std=c++0x -D_GLIBCXX_USE_NANOSLEEP -D_GLIBCXX_USE_SCHED_YIELD -I/usr/include/libxml2/ -I/usr/include
+
+# The LDFLAGS variable sets flags for linker
+#  -lm       link in libm (math library)
+#  -lpthread link in libpthread (thread library) to back C++11 extensions
+#  -lrand    link to simple random number generator
+#  -lthreads link to convenience functions layering over C++11 threads
+#  -lxml2    link in libxml2 (XML processing library)
+#LDFLAGS = -lm -lpthread -L/usr/class/cs110/local/lib -lrand -lthreads -L/usr/class/cs110/lib/libxml2 -lxml2
+#LDFLAGS = -lm -lpthread -L/usr/lib -lrand -lthreads -L/usr/include/libxml2/ -lxml2
+LDFLAGS = -lm -lpthread -L/usr/lib -L/usr/include/libxml2/ -lxml2
+
+# In this section, you list the files that are part of the project.
+# If you add/change names of header/source files, here is where you
+# edit the Makefile.
+SOURCES = \
+	news-aggregator.cc \
+	news-aggregator-utils.cc \
+	stream-tokenizer.cc \
+	rss-feed.cc \
+	rss-feed-list.cc \
+	html-document.cc \
+	rss-index.cc \
+
+HEADERS = \
+	article.h \
+	news-aggregator-utils.h \
+	stream-tokenizer.h \
+	rss-feed.h \
+	rss-feed-exception.h \
+	rss-feed-list.h \
+	rss-feed-list-exception.h \
+	html-document.h \
+	html-document-exception.h \
+	rss-index.h \
+
+OBJECTS = $(SOURCES:.cc=.o)
+TARGETS = news-aggregator
+
+default: $(TARGETS)
+
+news-aggregator: news-aggregator.o rss-feed.o rss-feed-list.o rss-index.o html-document.o stream-tokenizer.o news-aggregator-utils.o
+	$(CXX) $(CXXFLAGS) -o $@ $^ $(LDFLAGS)
+
+# In make's default rules, a .o automatically depends on its .c file
+# (so editing the .c will cause recompilation into its .o file).
+# The line below creates additional dependencies, most notably that it
+# will cause the .c to reocmpiled if any included .h file changes.
+
+Makefile.dependencies:: $(SOURCES) $(HEADERS)
+	$(CXX) $(CXXFLAGS) -MM $(SOURCES) > Makefile.dependencies
+
+-include Makefile.dependencies
+
+# Phony means not a "real" target, it doesn't build anything
+# The phony target "clean" that is used to remove all compiled object files.
+
+.PHONY: clean spartan
+
+clean:
+	@rm -f $(TARGETS) $(OBJECTS) core Makefile.dependencies news-aggregator_soln
+
+spartan: clean
+	@rm -f *~ .*~
---
+++ news-aggregator.cc
@@ -0,0 +1,148 @@
+/**
+ * File: news-aggregator.cc
+ * ------------------------
+ * When fully implemented, news-aggregator.cc pulls, parses,
+ * and indexes every single news article reachable from some
+ * RSS feed in the user-supplied RSS News Feed XML file,
+ * and then allows the user to query the index.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <libxml/parser.h>
+#include <libxml/catalog.h>
+#include <getopt.h>
+
+#include "article.h"
+#include "rss-feed-list.h"
+#include "rss-feed.h"
+#include "rss-index.h"
+#include "html-document.h"
+#include "html-document-exception.h"
+#include "rss-feed-exception.h"
+#include "rss-feed-list-exception.h"
+#include "news-aggregator-utils.h"
+//#include "string-utils.h"
+using namespace std;
+
+// globals
+static bool verbose = false;
+static RSSIndex index;
+
+static const int kIncorrectUsage = 1;
+
+extern string trim(string& str);
+
+static void printUsage(const string& message, const string& executableName) {
+  cerr << "Error: " << message << endl;
+  cerr << "Usage: " << executableName << " [--verbose] [--quiet] [--url <feed-file>]" << endl;
+  exit(kIncorrectUsage);
+}
+
+static const string kDefaultRSSFeedListURL = "small-feed.xml";
+static string parseArgumentList(int argc, char *argv[]) {
+  struct option options[] = {
+    {"verbose", no_argument, NULL, 'v'},
+    {"quiet", no_argument, NULL, 'q'},
+    {"url", required_argument, NULL, 'u'},
+    {NULL, 0, NULL, 0},
+  };
+
+  string url = kDefaultRSSFeedListURL;
+  while (true) {
+    int ch = getopt_long(argc, argv, "vqu:", options, NULL);
+    if (ch == -1) break;
+    switch (ch) {
+    case 'v':
+      verbose = true;
+      break;
+    case 'q':
+      verbose = false;
+      break;
+    case 'u':
+      url = optarg;
+      break;
+    default:
+      printUsage("Unrecognized flag.", argv[0]);
+    }
+  }
+
+  argc -= optind;
+  argv += optind;
+  if (argc > 0) {
+    printUsage("Too many arguments", argv[0]);
+  }
+  return url;
+}
+
+static const int kBogusRSSFeedListName = 1;
+static void processAllFeeds(const string& feedListURI) {
+  RSSFeedList feedList(feedListURI);
+  try {
+    feedList.parse();
+  } catch (const RSSFeedListException& rfle) {
+    cerr << "Ran into trouble while pulling full RSS feed list from \""
+	 << feedListURI << "\"." << endl;
+    cerr << "Aborting...." << endl;
+    exit(kBogusRSSFeedListName);
+  }
+
+  // note to student
+  // ---------------
+  // add well-decomposed code to read all of the RSS
+  // news feeds from feedList for their news articles,
+  // and for each news article URL, process it
+  // as an HTMLDocument and add all of the tokens
+  // to the master RSSIndex.
+}
+
+static const size_t kMaxMatchesToShow = 15;
+static void queryIndex() {
+  while (true) {
+    cout << "Enter a search term [or just hit <enter> to quit]: ";
+    string response;
+    getline(cin, response);
+    response = trim(response);
+    if (response.empty()) break;
+    const vector<pair<Article, int> >& matches = index.getMatchingArticles(response);
+    if (matches.empty()) {
+      cout << "Ah, we didn't find the term \"" << response << "\". Try again." << endl;
+    } else {
+      cout << "That term appears in " << matches.size() << " article"
+	   << (matches.size() == 1 ? "" : "s") << ".  ";
+      if (matches.size() > kMaxMatchesToShow)
+	cout << "Here are the top " << kMaxMatchesToShow << " of them:" << endl;
+      else if (matches.size() > 1)
+	cout << "Here they are:" << endl;
+      else
+        cout << "Here it is:" << endl;
+      size_t count = 0;
+      for (const pair<Article, int>& match: matches) {
+	if (count == kMaxMatchesToShow) break;
+	count++;
+	string title = match.first.title;
+	if (shouldTruncate(title)) title = truncate(title);
+	string url = match.first.url;
+	if (shouldTruncate(url)) url = truncate(url);
+	string times = match.second == 1 ? "time" : "times";
+	cout << "  " << setw(2) << setfill(' ') << count << ".) "
+	     << "\"" << title << "\" [appears " << match.second << " " << times << "]." << endl;
+	cout << "       \"" << url << "\"" << endl;
+      }
+    }
+  }
+}
+
+int main(int argc, char *argv[]) {
+  string rssFeedListURI = parseArgumentList(argc, argv);
+  xmlInitParser();
+  xmlInitializeCatalog();
+  processAllFeeds(rssFeedListURI);
+  xmlCatalogCleanup();
+  xmlCleanupParser();
+  queryIndex();
+  return 0;
+}
